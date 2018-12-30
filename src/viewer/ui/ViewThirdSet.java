32b8905dee25b88be7458ae451045a6e12392a61
Nuung <qlgks1@naver.com>
2018-12-30 16:40:08 +0900

    Use ex POI lib

--- src/viewer/ui/ViewThirdSet.java
+++ src/viewer/ui/ViewThirdSet.java
@@ -3,12 +3,20 @@ package viewer.ui;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import event.ButtonClickViewSecond;
 import viewer.ViewFrame;
@@ -26,10 +34,12 @@ public class ViewThirdSet {
 	public ViewThirdSet(ViewFrame viewFrame) {
 		this.viewFrame = viewFrame;
 		this.panelSetting();
-	}
+		this.RevisedFrame.transferFocus();
+	} // 생성자
+	
 	private void panelSetting() {
 		
-		RevisedFrame = new JFrame();
+		this.RevisedFrame = new JFrame();
 		p1 = new JPanel();
 		JTextField nameText = new JTextField();
 		JTextField idText = new JTextField();
@@ -37,6 +47,7 @@ public class ViewThirdSet {
 		JButton uploadBtn = new JButton("첨부파일");
 		JButton signupBtn = new JButton("가입하기");
 		
+		// Frame Setting
 		RevisedFrame.setSize(512, 512);
 		RevisedFrame.setVisible(true);
 		RevisedFrame.setLayout(null);
@@ -64,13 +75,67 @@ public class ViewThirdSet {
 		        if(butSrcTxt == "첨부파일") {
 		    		// test, making new frame
 		    		System.out.println("엑셀파일첨부");
-		        }
+		    		
+		    		// Reading Excel Files (XLSX 파일 리딩)
+		    		FileInputStream fis;
+		    		XSSFWorkbook workbook = null;
+		    		
+					try {
+						fis = new FileInputStream("test.xlsx");
+						workbook = new XSSFWorkbook(fis);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} // try catch
+		    		
+		    		//시트 수 (첫번째에만 존재하므로 0을 준다) -> 만약 각 시트를 읽기위해서는 FOR문을 한번더 돌려준다
+		    		XSSFSheet sheet = workbook.getSheetAt(0);
+		    		int rows = sheet.getPhysicalNumberOfRows(); // total 행의 수
+		    		int rowindex=0; // 행을 가르키는 index value
+		    		int columnindex=0; // 열을 가르키는 index value
+		    		
+		    		for(rowindex = 1; rowindex < rows; rowindex++){ // 각 행을 모두 읽는다
+		    			XSSFRow row = sheet.getRow(rowindex);
+		    			
+		    		    if(row !=null){ // 행이 비어 있지 않으면
+		    		        int cells = row.getPhysicalNumberOfCells(); // 행에 존재하는 total 셀 수
+		    		        for(columnindex = 0; columnindex <= cells; columnindex++){ // 셀 수 만큼 '열(컬럼값)'을 읽어온다
+		    		            
+		    		            XSSFCell cell = row.getCell(columnindex); //셀값을 읽는다
+		    		            String value = "";
+		    		        		    		            
+		    		            if(cell == null){ //셀이 빈값일경우를 위한 널체크
+		    		                continue;
+		    		            } else{ // 셀 타입별로 모두 다른 형태의 값이 된다 -> 셀 타입별로 모두 읽기
+		    		                switch (cell.getCellType()){
+		    		                case FORMULA:
+		    		                    value = cell.getCellFormula();
+		    		                    break;
+		    		                case NUMERIC:
+		    		                    value = cell.getNumericCellValue()+"";
+		    		                    break;
+		    		                case STRING:
+		    		                    value = cell.getStringCellValue()+"";
+		    		                    break;
+		    		                case BLANK:
+		    		                    value = cell.getBooleanCellValue()+"";
+		    		                    break;
+		    		                case ERROR:
+		    		                    value = cell.getErrorCellValue()+"";
+		    		                    break;
+		    		                } // switch()
+		    		            } // inner if - else
+		    		            System.out.println("각 셀 내용 :"+value);
+		    		        } // inner for
+		    		    } // if
+		    		} // for
+		        } // if 첨부파일
 		        else if(butSrcTxt == "가입하기") {
 			    	viewFrame.remove(p1); // delete 'p1' Panel
 			    	new ViewSecondSet(viewFrame); // Make Second Layout Setting
 			    	viewFrame.revalidate(); // ReLoading
 			    	RevisedFrame.dispose();
-		    	} 
+		    	} // if - else
 		    	
 		    } // btnAction
 		});
