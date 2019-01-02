35c0beb09fabc657a06072f722dd36f20fba8109
Nuung <qlgks1@naver.com>
2019-01-02 16:36:57 +0900

    event btn secondset

--- src/viewer/ui/SidePanel.java
+++ src/viewer/ui/SidePanel.java
@@ -6,19 +6,13 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-import event.ButtonClickViewSecond;
-
 public class SidePanel {
 	protected JPanel trackSidePanel;
 	protected JPanel simulSidePanel;
 	protected JPanel fidSidePanel;
 	protected JPanel infoSidePanel;
 	protected JPanel logOutSidePanel;
-
 	JButton tempBtn[];
-
-	// 이벤트처리
-	private ButtonClickViewSecond btnclick;
 
 	String[] sideTxt = { "HCI&비쥬얼컴퓨팅", "멀티미디어", "사물인터넷", "시스템응용", "인공지능", "가상현실", "정보보호", "데이터사이언스", "SW교육", "사이버국방" };
 
@@ -30,39 +24,32 @@ public class SidePanel {
 		logOutSidePanel = new JPanel();
 	} // 생성자
 
-	public Component trackSide() {
+	// 상단바 트랙버튼에 대한 사이드 바
+	public Component trackSide() { 
 		trackSidePanel.setLayout(new GridLayout(11, 1));
 		SideBarBtn(trackSidePanel);
-		btnListener(trackSidePanel, btnclick);
-
 		return trackSidePanel;
 	} // trackSidePanel()
 
 	public Component simulSide() {
 		simulSidePanel.setVisible(false);
-		btnListener(simulSidePanel, btnclick);
 		return simulSidePanel;
 	} // simulSidePanel()
 
 	public Component fidSide() {
 		fidSidePanel.setLayout(new GridLayout(11, 1));
 		SideBarBtn(fidSidePanel);
-		btnListener(fidSidePanel, btnclick);
-
 		return fidSidePanel;
 	} // simulSidePanel()
 
 	public Component infoSide() {
 		infoSidePanel.setLayout(new GridLayout(11, 1));
 		SideBarBtn(infoSidePanel);
-		btnListener(infoSidePanel, btnclick);
-
 		return infoSidePanel;
 	} // simulSidePanel()
 
 	public Component logOutSide() {
 		logOutSidePanel.setVisible(false);
-		btnListener(infoSidePanel, btnclick);
 		return logOutSidePanel;
 	} // simulSidePanel()
 
@@ -73,15 +60,5 @@ public class SidePanel {
 			tempPanel.add(tempBtn[i]);
 		}
 	} // SideBarBtn()
-
-	private void btnListener(JPanel panel, ButtonClickViewSecond btnclickEvent) {
-		btnclickEvent = new ButtonClickViewSecond(panel);
-		JButton[] btn = btnclickEvent.getButton();
-		// adding Event to All Button
-		for (int i = 0; i < btn.length; i++) {
-			btn[i].addActionListener(btnclickEvent);
-			System.out.println(btn[i].toString());
-		} // for
-	} // btn add Event
 
 } // SidePanel class
