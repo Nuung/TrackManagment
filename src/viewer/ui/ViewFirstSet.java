9e775422722349cb9050bd6b146b3eed71e72814
seongim <hsung325@gmail.com>
2019-01-02 14:34:49 +0900

    add SideBar UI

--- src/viewer/ui/ViewFirstSet.java
+++ src/viewer/ui/ViewFirstSet.java
@@ -8,16 +8,13 @@ import java.awt.event.ActionListener;
 import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 
 import controller.db.DBconnection;
 import viewer.ViewFrame;
 import viewer.components.PlaceholderJPasswordField;
 import viewer.components.PlaceholderJTextField;
+
 /*
  * 가장 첫 화면 [ 학생 ] [ 관리자 ]
  * 클릭시 newFrame -> 로그인
@@ -31,7 +28,7 @@ public class ViewFirstSet {
 	private JButton btn1, btn2;
 	private PlaceholderJTextField idText;
 	private PlaceholderJPasswordField passText;
-	
+
 	// 생성자
 	public ViewFirstSet(ViewFrame viewFrame) {
 		this.viewFrame = viewFrame;
@@ -40,38 +37,39 @@ public class ViewFirstSet {
 		this.viewFrame.add(this.p1);
 		this.p1.setBounds(100, 100, 800, 256);
 	}
-	
+
 	// Private Panel Setting
 	private void btnSetting() {
 		this.p1 = new JPanel();
 		this.p1.setLayout(new GridLayout(1, 2, 100, 0));
-		
+
 		this.btn1 = new JButton("학생");
 		this.btn2 = new JButton("관리자");
-		this.p1.add(btn1); this.p1.add(btn2);
+		this.p1.add(btn1);
+		this.p1.add(btn2);
 		// adding event
 		this.btnAction(this.btn1);
 	}
-	
+
 	// Private New Frame For Loging and SignUp
 	void newFrameSet() {
 		newFrame = new JFrame();
 		JPanel tempP = new JPanel();
-		
+
 		this.idText = new PlaceholderJTextField("");
 		idText.setPlaceholder("Student Num");
 		Font f = idText.getFont();
 		idText.setFont(new Font(f.getName(), f.getStyle(), 40));
 		this.passText = new PlaceholderJPasswordField("");
 		passText.setPlaceholder("Passward");
-        passText.setFont(new Font(f.getName(), f.getStyle(), 40));
+		passText.setFont(new Font(f.getName(), f.getStyle(), 40));
 		JButton loginBtn = new JButton("로그인");
 		JButton signupBtn = new JButton("회원가입");
-	
+
 		// adding event
 		this.btnAction(loginBtn);
 		this.btnAction(signupBtn);
-		
+
 		newFrame.setSize(512, 512);
 		newFrame.setVisible(true);
 		newFrame.setLayout(null);
@@ -83,39 +81,36 @@ public class ViewFirstSet {
 		tempP.add(signupBtn);
 		newFrame.add(tempP);
 	} // newFrameSet()
-	
+
 	// 각 버튼의 액션 이벤트 세팅과 명시
 	private void btnAction(JButton inBtn) {
-		inBtn.addActionListener ( new ActionListener() {
-		    public void actionPerformed(ActionEvent ev) {
-		    	
+		inBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+
 				// getting btn text value
-				Object source =  ev.getSource();
-		        String butSrcTxt = ((AbstractButton) source).getText();
-		    	
-		        if(butSrcTxt == "학생") {
-		    		// test, making new frame
-		    		newFrameSet();
-		        }
-		        else if(butSrcTxt == "로그인") {
-		        	DBconnection dbcon = new DBconnection();
-		        	// TO DO
-		        	
-		        	
-			    	viewFrame.remove(p1); // delete 'p1' Panel
-			    	new ViewSecondSet(viewFrame); // Make Second Layout Setting			   
-			    	// New Frame이 아니라, 기존에 있는 Frame Re Setting -> ReLoading
-			    	viewFrame.revalidate(); // ReLoading
-			    	newFrame.dispose();
-		    	} 
-		        else if(butSrcTxt == "회원가입") {
-		        	viewFrame.remove(p1);
-		        	// SignUp button --> 새로운 프레임 띄우기
-		        	new ViewThirdSet(viewFrame); // -> 실제 이벤트 액션은 ThirdSet에서 명시
-		    		newFrame.dispose();
-		    	}
-		    	
-		    } // actionPerformed()
+				Object source = ev.getSource();
+				String butSrcTxt = ((AbstractButton) source).getText();
+
+				if (butSrcTxt == "학생") {
+					// test, making new frame
+					newFrameSet();
+				} else if (butSrcTxt == "로그인") {
+					DBconnection dbcon = new DBconnection();
+					// TO DO
+
+					viewFrame.remove(p1); // delete 'p1' Panel
+					new ViewSecondSet(viewFrame); // Make Second Layout Setting
+					// New Frame이 아니라, 기존에 있는 Frame Re Setting -> ReLoading
+					viewFrame.revalidate(); // ReLoading
+					newFrame.dispose();
+				} else if (butSrcTxt == "회원가입") {
+					viewFrame.remove(p1);
+					// SignUp button --> 새로운 프레임 띄우기
+					new ViewThirdSet(viewFrame); // -> 실제 이벤트 액션은 ThirdSet에서 명시
+					newFrame.dispose();
+				}
+
+			} // actionPerformed()
 		}); // addActionListener
 	} // btnAction()
 
--- src/viewer/ui/ViewSecondSet.java
+++ src/viewer/ui/ViewSecondSet.java
@@ -1,6 +1,7 @@
 package viewer.ui;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -8,59 +9,78 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-import viewer.ViewFrame;
 import event.ButtonClickViewSecond;
+import viewer.ViewFrame;
+
 /*
- * 가장 메인 화면 MainFrame!
- * 상단 메뉴, 네비게이션 메뉴 동적 생성 필요
+ * 媛��옣 硫붿씤 �솕硫� MainFrame!
+ * �긽�떒 硫붾돱, �꽕鍮꾧쾶�씠�뀡 硫붾돱 �룞�쟻 �깮�꽦 �븘�슂
  */
 public class ViewSecondSet {
-	
+
 	// Default Swing
 	private ViewFrame viewFrame;
 	private JPanel p1, p2, p3;
 	private ButtonClickViewSecond btnclick;
-	private String topBar[] = {"트랙", "트랙시물레이션", "피드백", "INFO", "LogOut"};
-	String trackName[] = {"HCI&비쥬얼컴퓨팅", "멀티미디어", "사물인터넷", "시스템응용", "인공지능", "가상현실", "정보보호", "데이터사이언스", "SW교육", "사이버국방"};
-	
-	
+	private String topBar[] = { "�듃�옓", "�듃�옓�떆臾쇰젅�씠�뀡", "�뵾�뱶諛�", "INFO", "LogOut" };
+	String trackName[] = { "HCI&鍮꾩�ъ뼹而댄벂�똿", "硫��떚誘몃뵒�뼱", "�궗臾쇱씤�꽣�꽬", "�떆�뒪�뀥�쓳�슜", "�씤怨듭��뒫", "媛��긽�쁽�떎", "�젙蹂대낫�샇",
+			"�뜲�씠�꽣�궗�씠�뼵�뒪", "SW援먯쑁", "�궗�씠踰꾧뎅諛�" };
+
+	// sideBar
+	CardLayout cardLayout;
+	SidePanel sidePanel;
+
 	public ViewSecondSet(ViewFrame viewFrame) {
 		this.viewFrame = viewFrame;
 		this.panelSetting();
 	}
-	
+
 	// setting
 	private void panelSetting() {
 		this.p1 = new JPanel();
 		this.p2 = new JPanel();
 		this.p3 = new JPanel();
-		
+
 		this.p1.setLayout(new GridLayout(1, 5));
 		this.p1.setName("p1");
-		this.btnBarSetting(this.p1, 5); // p1 패널에 5개 버튼 추가
+		this.btnBarSetting(this.p1, 5); // p1 �뙣�꼸�뿉 5媛� 踰꾪듉 異붽�
 		this.btnAddListener(this.p1, btnclick);
-		
-		this.p2.setLayout(new GridLayout(10, 1));
-		this.p2.setName("p2");
-		this.btnBarSetting(this.p2, 5); // p2 패널에 5개 버튼 추가
-		this.btnAddListener(this.p2, btnclick);
-		
-	//	this.p2.setVisible(false); // topBar(트랙, 피드백, INFO) 클릭시 setVisible(true)
-		
+
+//		this.p2.setLayout(new GridLayout(10, 1));
+//		this.p2.setName("p2");
+//		this.btnBarSetting(this.p2, 5); // p2 패널에 5개 버튼 추가
+//		this.btnAddListener(this.p2, btnclick);
+
+		// p2패널 - sideBar
+		cardLayout = new CardLayout();
+		sidePanel = new SidePanel();
+		this.p2.setLayout(cardLayout);
+		sidePanel.trackSide();
+		this.p2.add(sidePanel.trackSidePanel, "tP");
+		this.p2.add(sidePanel.simulSide(), "sP");
+		this.p2.add(sidePanel.fidSide(), "fP");
+		this.p2.add(sidePanel.infoSide(), "iP");
+		this.p2.add(sidePanel.logOutSide(), "lP");
+
+		cardLayout.show(p2, "tP");
+
+		// this.p2.setVisible(false); // topBar(�듃�옓, �뵾�뱶諛�, INFO) �겢由��떆
+		// setVisible(true)
+
 		this.p3.setLayout(new GridLayout(1, 1));
 		JScrollPane jsp = new JScrollPane(new JTextArea("TEST"));
-		this.p3.add(jsp); // p3 패널은 article 부분 (MAIN)
-		
+		this.p3.add(jsp); // p3 �뙣�꼸�� article 遺�遺� (MAIN)
+
 		// Main Frame Setting
 		ArticleUIpanel au = new ArticleUIpanel();
-		
+
 		this.viewFrame.setLayout(new BorderLayout());
 		this.viewFrame.add(this.p1, BorderLayout.NORTH);
 		this.viewFrame.add(this.p2, BorderLayout.WEST);
-	//	this.viewFrame.add(this.p3, BorderLayout.CENTER);
-		this.viewFrame.add(au, BorderLayout.CENTER);	
+		// this.viewFrame.add(this.p3, BorderLayout.CENTER);
+		this.viewFrame.add(au, BorderLayout.CENTER);
 	} // panelSetting()
-	
+
 	public void btnBarSetting(JPanel tempP, int btnNum) {
 		JButton tempBtn[] = new JButton[btnNum];
 		for (int i = 0; i < tempBtn.length; i++) {
@@ -69,12 +89,12 @@ public class ViewSecondSet {
 		} // for
 	} // btnBarSetting()
 
-	// Inner Btn Action -> 간편하게 우선 익명함수로 만들어둠
+	// Inner Btn Action -> 媛꾪렪�븯寃� �슦�꽑 �씡紐낇븿�닔濡� 留뚮뱾�뼱�몺
 	private void btnAddListener(JPanel panel, ButtonClickViewSecond btnclickEvent) {
 		btnclickEvent = new ButtonClickViewSecond(panel);
 		JButton[] btn = btnclickEvent.getButton();
 		// adding Event to All Button
-		for(int i=0; i<btn.length; i++) {
+		for (int i = 0; i < btn.length; i++) {
 			btn[i].addActionListener(btnclickEvent);
 		} // for
 	} // btn add Event
