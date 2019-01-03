6d82c3beb47152b619387bac3719a23308e3c1e2
unknown <aorwn212@naver.com>
2019-01-03 22:09:11 +0900

    뷰 프레임, 런처, first set에 주석 추가

--- src/viewer/ViewFrame.java
+++ src/viewer/ViewFrame.java
@@ -11,9 +11,10 @@ import javax.swing.UIManager;
 
 import viewer.ui.ViewFirstSet;
 
+// 시스템의 전체 화면을 담아줄 클래스, JFrame을 상속받음
 public class ViewFrame extends JFrame{
 
-	// Size of Frame
+	// Size of Frame, 프레임의 사이즈, 2:1 비율
 	private final int WIDTH = 1024, HEIGHT = 512;
 	
 	public ViewFrame() {
@@ -29,6 +30,7 @@ public class ViewFrame extends JFrame{
 		this.setVisible(true);
 	} // ViewFrame()
 
+	// 버튼 등과 같은 컴포넌트들을 좀 더 예쁘게 처리하기 위해 UIManager를 적용시키기 위한 메서드
 	private void SettingLookAndFeel() {
 		// look and fell setting
 		try {
